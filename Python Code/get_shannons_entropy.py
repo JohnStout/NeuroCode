150e3503fdcb06e3a36f273ec9f7502b90f0f13e
John Stout <john.j.stout.jr@gmail.com>
2020-10-20 10:52:12 -0400

    this entropy is not correct

--- Python Code/get_shannons_entropy.py
+++ Python Code/get_shannons_entropy.py
@@ -13,20 +13,20 @@ https://www.kite.com/python/answers/how-to-calculate-shannon-entropy-in-python
 @author: JS
 """
 
-# this needs to made into a function that can be called into matlab
+def shannons_entropy(data):
+    
+    # this needs to made into a function that can be called into matlab
+    import pandas as pd
+    import scipy.stats as stat
+    #import scipy.io as sio
 
-import pandas as pd
-import scipy.stats as stat
-import scipy.io as sio
+    # shannons entropy
+    #mat       = sio.loadmat('testing_entropy.mat', squeeze_me=True)
+    #data      = mat["binned_data"]
+    pd_series = pd.Series(data)
+    counts    = pd_series.value_counts()
+    entropy   = stat.entropy(counts)
 
-
-# shannons entropy
-mat       = sio.loadmat('testing_entropy.mat', squeeze_me=True)
-data      = mat["binned_data"]
-pd_series = pd.Series(data)
-counts    = pd_series.value_counts()
-entropy   = stat.entropy(counts)
-
-
+    return entropy
 
 
